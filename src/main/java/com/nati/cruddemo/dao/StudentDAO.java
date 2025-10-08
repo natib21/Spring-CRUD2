619b3673d04beae9d4c201835a0ecd84f7bd83b3
Nathnael <nathnaelzelalem@gmail.com>
2025-10-08 09:27:15 +0300

    finalaise system

--- src/main/java/com/nati/cruddemo/dao/StudentDAO.java
+++ src/main/java/com/nati/cruddemo/dao/StudentDAO.java
@@ -4,6 +4,17 @@ import com.nati.cruddemo.entity.Student;
 import java.util.List;
 public interface StudentDAO {
     void save(Student theStudent);
+
     Student findById(Integer id);
+
     List<Student> findAll();
+
+    List<Student> findByLastName(String thelastName);
+
+    void update(Student theStudent);
+
+    void delete(Integer id);
+
+    int deleteAll();
+
 }
